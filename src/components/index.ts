a57d4e013f87278eeeaf983f9da7b6d40426931c
Mohsin <122507740+Mohsin-mw@users.noreply.github.com>
2023-08-12 02:36:25 +0500

    Finish Landing Page / Enhance Codebase
    * Initial Commit
    
    * Add File Structure
    
    * Loader Added
    
    * Loading Provider Added
    
    * Landing Page Layout Added
    
    * Landing Page Code Cleaned
    
    * Footer Added
    
    * Documentation Added
    
    * main.tsx documented

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Landing/Navbar";
+export { default as Hero } from "./Landing/Hero";
+export { default as Feature } from "./Landing/Feature";
+export { default as FeatureImage } from "./Landing/FeatureImage";
+export { default as FeatureText } from "./Landing/FeatureText";
+export { default as Footer } from "./Landing/Footer";
