a03aaf85b1472ecc157ec2095f5c1bdbb11f4c2d
MMW <null.mmw@gmail.com>
2023-08-12 02:32:10 +0500

    main.tsx documented

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,19 @@ import "./index.css";
 // Import Loader Context Provider
 import { LoadingProvider } from "./context/LoaderProvider.tsx";
 
+/**
+ * Entry point of the application.
+ *
+ * This code initializes the React application by rendering the root component within the specified DOM element.
+ * It wraps the <App /> component with a <LoadingProvider /> to provide loading state management using context.
+ *
+ * @function
+ * @returns {void}
+ */
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    {/* Wrap the App component with LoadingProvider for loading state management */}
     <LoadingProvider>
       <App />
     </LoadingProvider>
