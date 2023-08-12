330ab5fede6269f9b5d11c85b38f174066468740
MMW <null.mmw@gmail.com>
2023-08-13 00:26:05 +0500

    Add Error Page

--- src/components/Authentication/shared/Image.tsx
+++ src/components/Authentication/shared/Image.tsx
@@ -21,7 +21,7 @@
  */
 const Image = (): JSX.Element => {
   return (
-    <div className="relative hidden   flex-1 lg:flex items-center justify-center w-full bg-cover bg-no-repeat bg-[url(https://i.ibb.co/98PdJGg/Server-Invite-Splash-Background.png)]   ">
+    <div className="relative hidden   flex-1 lg:flex items-center justify-center w-full bg-cover bg-no-repeat bg-right bg-[url(https://i.ibb.co/nn1zMZZ/Discovery-Banner.png)]   ">
       <div className=" flex flex-col items-center justify-center  h-full w-full bg-black bg-opacity-30 ">
         <div className="text-white  text-9xl tracking-widest drop-shadow-md font-extrabold font-passion-one">
           DISCORD
