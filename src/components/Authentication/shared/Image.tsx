21e16a27ff57949b45aef6165499a24bb5fb5f52
MMW <null.mmw@gmail.com>
2023-08-13 00:02:52 +0500

    Auth Pages Image Changed

--- src/components/Authentication/shared/Image.tsx
+++ src/components/Authentication/shared/Image.tsx
@@ -21,9 +21,9 @@
  */
 const Image = (): JSX.Element => {
   return (
-    <div className="relative hidden   flex-1 lg:flex items-center justify-center w-full bg-cover bg-no-repeat bg-[url(https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80)]   ">
-      <div className=" flex flex-col items-center justify-center backdrop-blur-sm h-full w-full bg-black bg-opacity-30 ">
-        <div className="text-white  text-9xl tracking-widest drop-shadow-md font-extrabold">
+    <div className="relative hidden   flex-1 lg:flex items-center justify-center w-full bg-cover bg-no-repeat bg-[url(https://i.ibb.co/98PdJGg/Server-Invite-Splash-Background.png)]   ">
+      <div className=" flex flex-col items-center justify-center  h-full w-full bg-black bg-opacity-30 ">
+        <div className="text-white  text-9xl tracking-widest drop-shadow-md font-extrabold font-passion-one">
           DISCORD
           <span className="opacity-70">.</span>
         </div>
